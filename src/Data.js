d7d84d6ad6a5f1f73487828f9a823de3c1de4db2
Italo Fabrício <italofps65@gmail.com>
2024-08-29 18:03:10 -0300

    create database

---
+++ src/Data.js
@@ -0,0 +1,34 @@
+export const news = [
+  {
+    title: "Noticia 1",
+    text: " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    image:
+      "https://media.beehiiv.com/cdn-cgi/image/fit=scale-down,format=auto,onerror=redirect,quality=80/uploads/asset/file/d3f71a9d-42e0-45ae-9c46-60247138eff0/image.png?t=1724701846",
+    likes: 2,
+    comments: 8,
+  },
+  {
+    title: "Noticia 2",
+    text: " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    image:
+      "https://media.beehiiv.com/cdn-cgi/image/fit=scale-down,format=auto,onerror=redirect,quality=80/uploads/asset/file/d3f71a9d-42e0-45ae-9c46-60247138eff0/image.png?t=1724701846",
+    likes: 2,
+    comments: 8,
+  },
+  {
+    title: "Noticia 3",
+    text: " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    image:
+      "https://media.beehiiv.com/cdn-cgi/image/fit=scale-down,format=auto,onerror=redirect,quality=80/uploads/asset/file/d3f71a9d-42e0-45ae-9c46-60247138eff0/image.png?t=1724701846",
+    likes: 2,
+    comments: 8,
+  },
+  {
+    title: "Noticia 4",
+    text: " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    image:
+      "https://media.beehiiv.com/cdn-cgi/image/fit=scale-down,format=auto,onerror=redirect,quality=80/uploads/asset/file/d3f71a9d-42e0-45ae-9c46-60247138eff0/image.png?t=1724701846",
+    likes: 2,
+    comments: 8,
+  },
+];
