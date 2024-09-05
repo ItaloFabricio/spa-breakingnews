d23e83c3b4f75f1f9de1ce2a96a92965c546f3a5
Italo Fabrício <italofps65@gmail.com>
2024-09-05 16:32:15 -0300

    searching for title in the API and showing response to search results that don't exist

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,50 @@
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { searchNews } from "../../services/newsServices";
+import { ContainerResults, SearchNews, TextResults } from "./SearchStyled";
+import { Card } from "../../components/Cards/Card";
+
 
 export function Search() {
-    const {title} = useParams();
+  const { title } = useParams(); //hook que vai puxar o titulo que vai vir como parametro
+  const [news, setNews] = useState([]);
 
-    return <h1>{title}</h1>
+  async function search() {
+    try {
+      const newsApi = await searchNews(title);
+      setNews(newsApi.data.results);
+      console.log("Resposta da API:", newsApi);
+      console.log("Dados da API:", newsApi.data);
+      console.log("Resultados:", newsApi.data.results);
+    } catch (err) {
+      setNews([]);
+    }
+  }
+
+  useEffect(() => {
+    search();
+  }, [title]);
+
+  return (
+    <ContainerResults>
+      <TextResults>
+        <span>
+          {news.length ? `Encontramos ${news.length} ${news.length > 1 ? "resultados" : "resultado"} para` : "Não encontramos resultados para:"}
+        </span>
+        <h2>{title}</h2>
+      </TextResults>
+      <SearchNews>
+        {news.map((item) => (   
+            <Card
+              key={item.id}
+              title={item.title}
+              text={item.text}
+              banner={item.banner}
+              likes={item.likes}
+              comments={item.comments}
+            />
+        ))}
+      </SearchNews>
+    </ContainerResults>
+  );
 }
--- src/pages/Search/SearchStyled.jsx
+++ src/pages/Search/SearchStyled.jsx
@@ -1,1 +1,27 @@
 import styled from "styled-components";
+
+export const ContainerResults = styled.section`
+    padding-top: 1rem;
+    width: 100%;
+    max-width: 1440px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin: 0 auto;
+    img {
+        width: 50%;
+    }
+`;
+
+export const SearchNews = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    
+    margin: 1rem auto;
+`;
+
+export const TextResults = styled.div`
+    
+`;
