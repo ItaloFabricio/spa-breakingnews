07e47ca09b581193101e9a9d6c14a14362c486c8
Italo Fabrício <italofps65@gmail.com>
2024-09-17 17:44:38 -0300

    feat: styling the news page

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getNewsById } from "../../services/newsServices";
+import { Comments, NewsComment, NewsContainer, NewsContent } from "./NewsPageStyled";
+import { TextLimit } from "../../components/TextLimit/TextLimit";
 
 export function NewsPage() {
   const { id } = useParams(); // Captura o ID da notícia da URL
@@ -8,13 +10,13 @@ export function NewsPage() {
   const [news, setNews] = useState(null); // Inicializa com null pois será um único objeto
 
   // Função para buscar a notícia pelo ID
-  async function findNewsById(newsId) {
+  async function findNewsById(id) {
     try {
       const { data } = await getNewsById(id);
       console.log(data.news.title);
       setNews(data.news);
       console.log(data);
-      console.log('Comentários:', data.news.comments); // Adicione esta linha
+      console.log('Comentários:', data.news.comments);
     } catch (error) {
       console.error("Error fetching news:", error);
     }
@@ -25,44 +27,50 @@ export function NewsPage() {
     if (id) {
       findNewsById(id); // Chama a função apenas se o ID estiver disponível
     }
-  }, [id]);
+  }, []);
 
   // Renderiza a página da notícia
   return (
-    <div>
-      {news ? ( // Verifica se os dados da notícia estão disponíveis
-        <>
-          <h1>{news.title}</h1>
-          <p>{news.text}</p>
-          <img src={news.banner} alt={news.title} />
-          <p>Autor: {news.userName}</p>
-          <div>
-            <h2>Comentários:</h2>
-            {Array.isArray(news.comments) && news.comments.length > 0 ? (
-              news.comments.map((commentObj, index) => (
-                <div key={index}>
-                  <p>{commentObj.comment?.comment || 'Comentário inválido'}</p>
-                  <p>Autor: {commentObj.userId || 'Desconhecido'}</p>
-                </div>
-              ))
-            ) : (
-              <p>Não há comentários para esta notícia.</p>
-            )}
-          </div>
-          <div>
-            <h2>Likes:</h2>
-            {news.likes.map((like) => (
-              <div key={like._id}>
-                <p>
-                  Usuário {like.userId} deu like em {like.created}
-                </p>
+    <NewsContainer>
+      <NewsContent>
+        {news ? ( // Verifica se os dados da notícia estão disponíveis
+          <>
+            <h1>{news.title}</h1>
+            <p>Autor: {news.userName}</p>
+            <img src={news.banner} alt={news.title} />
+            <p>{news.text}</p>
+            <NewsComment>
+              <div>
+                <h2>Comentários</h2>
+                <Comments>
+                  {Array.isArray(news.comments) && news.comments.length > 0 ? (
+                    news.comments.map((commentObj, index) => (
+                      <div key={index}>
+                        <p>{commentObj.comment?.comment || 'Comentário inválido'}</p>
+                        <p>Autor: {commentObj.userId || 'Desconhecido'}</p>
+                      </div>
+                    ))
+                  ) : (
+                    <p>Não há comentários para esta notícia.</p>
+                  )}
+                </Comments>
               </div>
-            ))}
-          </div>
-        </>
-      ) : (
-        <p>Loading...</p> // Exibe um indicador de carregamento enquanto os dados são buscados
-      )}
-    </div>
+            </NewsComment>
+            <div>
+              <h2>Likes:</h2>
+              {news.likes.map((like) => (
+                <div key={like._id}>
+                  <p>
+                    Usuário {like.userId} deu like em {like.created}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <p>Loading...</p> // Exibe um indicador de carregamento enquanto os dados são buscados
+        )}
+      </NewsContent>
+    </NewsContainer>
   );
 }
--- src/pages/NewsPage/NewsPageStyled.jsx
+++ src/pages/NewsPage/NewsPageStyled.jsx
@@ -1,1 +1,46 @@
 import styled from "styled-components";
+
+export const NewsContainer = styled.section`
+  width: 100%;
+  max-width: 1440px;
+  margin: 1rem auto;
+  padding: 4rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border-radius: 0.3rem;
+
+  h1 {
+    margin-bottom: .8rem;
+    font-size: 2rem;
+    color: #1F2123;
+  }
+`;
+
+export const NewsContent = styled.div`
+    width: 40%;
+
+    img{
+        width: 600px;
+        height: 400px;
+        margin: 1rem 0;
+        border-radius: 12px;
+    }
+`;
+
+export const NewsComment = styled.div`
+    margin: 4rem 0;
+
+    h2 { 
+        color: #1F2123;
+        margin-bottom: 1rem;
+    }
+`;
+
+export const Comments = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
